6f99fb5b136210e547c4e25e0bb68767fae7388f
Ian Oberbillig <Ian@Ians-MacBook-Pro-2.local>
2018-04-09 12:01:48 -0600

    built mongodb and initial user routes, also built userSchema

--- Comp/MyPage.js
+++ Comp/MyPage.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import { userName } from './Login.js';
 
 export default class MyPage extends React.Component {
   render() {
     const { params } = this.props.navigation.state;
     const userName = params ? params.userName : null;
+    const password = params ? params.password : null;
 
     return(
       <View>
         <Text>Welcome back, {userName}!</Text>
+        <Text> your password is {password}</Text>
       </View>
     )
   }
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+const UserSchema = require('./models/UserSchema.js');
+const router = express.Router();
+
+mongoose.connect('mongodb://localhost/user-auth');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+router.use((req, res, next) => {
+  console.log('something is happening');
+  next();
+});
+
+router.get('/', (req, res) => {
+  res.json({message: 'yay our database works'});
+});
+
+router.route('/userId')
+  .post((req, res) => {
+    const userId = new UserSchema();
+    userId.username = req.body.username;
+    userId.password = req.body.password;
+    userId.save(err => {
+      if(err)
+        res.send(err);
+      res.json({message: 'new user!'});
+    });
+  })
+  .get((req, res) => {
+    UserSchema.find((err, userId) => {
+      if(err)
+        res.send(err);
+      res.json(userId);
+    });
+  });
+
+app.use('/api', router);
+app.listen(port);
+console.log(`magic happens on port ${port}`);
